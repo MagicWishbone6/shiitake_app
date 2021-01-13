dcd67796f6cd16fef787a03dd6a177922441acdc
magicwishbone6 <caitlin.cmo@gmail.com>
2021-01-13 13:49:04 -0600

    bootstrap and components in

---
+++ shiitake/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header(props) {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg='light' variant='light'>
+			<Navbar.Brand href='/' id='navTitle'>shiitake!</Navbar.Brand>
+			<Nav fill className='mr-auto' variant='pills' defaultActiveKey='/home'>
+				<Nav.Link id='homeNav' href='/home'>Home</Nav.Link>
+			</Nav>
+		</Navbar>
+    );
+}
+
+export default Header;
