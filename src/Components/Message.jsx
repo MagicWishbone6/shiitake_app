9e19d4172c27432634c0c252d69fc56c75d18e6d
Caitlin Lamprecht <caitlin.lamprecht@edquity.co>
2021-10-17 18:40:45 -0500

    moved duck replacer to utils

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,22 +1,10 @@
 import React from "react";
+import { duckReplacement } from "../utilities/duckReplacement";
 
 function Message({ message }) {
-	let title = message.name;
+	let title = message.name
 
-	const duckReplacement = () => {
-		if (title.indexOf("Fuck") > -1) {
-			title = title.replace(`Fuck`, `Duck`);
-			duckReplacement();
-		} else if (title.indexOf("fuck") > -1) {
-			title = title.replace("fuck", "duck");
-			duckReplacement();
-		} else if (title.length > 50) {
-			title = title.slice(0, 50);
-		} else {
-		}
-	};
-
-	duckReplacement();
+	title = duckReplacement(title);
 	const path = message.url;
 
 	const pathFilter = () => {
---
+++ src/utilities/duckReplacement.jsx
@@ -0,0 +1,14 @@
+export function duckReplacement(text) {
+    text = text + ""
+    if (text.indexOf("Fuck") > -1) {
+        text = text.replace(`Fuck`, `Duck`);
+        duckReplacement();
+    } else if (text.indexOf("fuck") > -1) {
+        text = text.replace("fuck", "duck");
+        duckReplacement();
+    } else if (text.length > 50) {
+        text = text.slice(0, 50);
+    } else {
+    }
+    return text
+}
